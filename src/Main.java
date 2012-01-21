64ff57e3e1dae62af28b8152f8374621d70afb52
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-01-21 06:54:11 +0000

    코드 깔금하게 수정

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,20 @@ public class Main {
 
 		SmingGo interpreter = SmingGo.getInstance();
 		
-		Object ob1 = new PrintTingWords();
-		Object ob2 = new StackWords();
-		Object ob3 = new StringWords();
-		Object ob4 = new DefineWords();
-		Object ob5 = new ListWords();
-		Object ob6 = new MathWords();
-		Object ob7 = new ControlWords();
-		Object ob8 = new BooleanWords();
-		Object ob9 = new CommentWords();
-		Object ob10 = new IOWords();
-		Object ob11 = new CheckWords();
-		Object ob12 = new ConvertWords();
-		Object ob13 = new ProcessingWords();
+		new PrintTingWords();
+		new StackWords();
+		new StringWords();
+		new DefineWords();
+		new ListWords();
+		new MathWords();
+		new ControlWords();
+		new BooleanWords();
+		new CommentWords();
+		new IOWords();
+		new CheckWords();
+		new ConvertWords();
+		new ProcessingWords();
 		
-		long start = System.currentTimeMillis();
 		interpreter.run("  [ 255 255 3 background 0 0 0 pstack ]  setup ");
-		
-		long end = System.currentTimeMillis();
-
-		System.out.println( "실행 시간 : " + ( end - start )/1000.00 ); 
 	}
 }
