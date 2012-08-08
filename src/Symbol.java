e2250ad3f2dc78862ef80914efb05e9518378d53
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-08 13:26:59 +0000

    심볼 객체 주석 추가!

--- src/Symbol.java
+++ src/Symbol.java
@@ -1,9 +1,10 @@
-
 public class Symbol {
-	private String symbol;
+	//스밍고에서 심볼로 쓰일 객체!
+  private String symbol; // 표시될 문자열이 저장되는 부분
 	
 	public Symbol(String str) {
 		// TODO Auto-generated constructor stub
+    // 심볼 객체는 문자열을 받아 심볼객체를 만듬!
 		this.symbol = str;
 	}
 	@Override
