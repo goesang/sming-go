6c725f2eaaae4759f9a1e94babd3b2a16acd879a
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-08 13:40:55 +0000

    기본 자료형 변환 단어 추가 및 주석 추가

--- src/ConvertWords.java
+++ src/ConvertWords.java
@@ -1,8 +1,8 @@
-//////////// 만드셈!!!!
 public class ConvertWords {
+//기본 자료형 변환에 관한 단어들
 	public ConvertWords(){
-	    PrimDict.getInstance().put("str-int",new PrimWord() {
-			
+	    PrimDict.getInstance().put("str->int",new PrimWord() {
+			//문자열에서 정수형으로 변환
 			@Override
 			public Object excute() {
 				// TODO Auto-generated method stub
@@ -13,12 +13,12 @@ public class ConvertWords {
 			@Override
 			public String toString() {
 				// TODO Auto-generated method stub
-				return "str-int";
+				return "str->int";
 			}
 		});
 	    
-	    PrimDict.getInstance().put("str-float",new PrimWord() {
-			
+	    PrimDict.getInstance().put("str->float",new PrimWord() {
+ 			//문자열에서 부동소수형으로 변환  		
 			@Override
 			public Object excute() {
 				// TODO Auto-generated method stub
@@ -29,12 +29,12 @@ public class ConvertWords {
 			@Override
 			public String toString() {
 				// TODO Auto-generated method stub
-				return "str-float";
+				return "str->float";
 			}
 		});	
 	    
-	    PrimDict.getInstance().put("str-bool",new PrimWord() {
-			
+	    PrimDict.getInstance().put("str->bool",new PrimWord() {
+ 			//문자열에서 부울대수형으로 변환  		
 			@Override
 			public Object excute() {
 				// TODO Auto-generated method stub
@@ -45,8 +45,199 @@ public class ConvertWords {
 			@Override
 			public String toString() {
 				// TODO Auto-generated method stub
-				return "str-bool";
+				return "str->bool";
+			}
+		}); 
+      
+	    PrimDict.getInstance().put("str->sym",new PrimWord() {
+ 			//문자열에서 심볼형으로 변환  		
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "str->sym";
+			}
+		});	    
+	    PrimDict.getInstance().put("int->str",new PrimWord() {
+			//정수형에서 문자열로 변환 
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "int->str";
+			}
+		});
+	    
+	    PrimDict.getInstance().put("int->float",new PrimWord() {
+			//정수형에서 부동소수형으로 변환
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "int->float";
 			}
 		});	
+	    
+	    PrimDict.getInstance().put("float->int",new PrimWord() {
+			//부동소수형에서 정수형으로 변환
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "float->int";
+			}
+		}); 
+      
+	    PrimDict.getInstance().put("float->str",new PrimWord() {
+			//부동소수형에서 문자열로 변환
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "float->str";
+			}
+		}); 
+	    PrimDict.getInstance().put("float->sym",new PrimWord() {
+			//부동소수형에서 심볼형으로 변환
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "float->sym";
+			}
+		});
+	    
+	    PrimDict.getInstance().put("sym->int",new PrimWord() {
+			//심볼형에서 정수형으로 변환
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "sym->int";
+			}
+		});	
+	    
+	    PrimDict.getInstance().put("sym->float",new PrimWord() {
+			//심볼형에서 부동소수형으로 변환			
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "sym->float";
+			}
+		}); 
+      
+	    PrimDict.getInstance().put("sym->str",new PrimWord() {
+			//심볼형에서 문자열로 변환  			
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "sym->str";
+			}
+		}); 
+	    PrimDict.getInstance().put("sym->bool",new PrimWord() {
+			//심볼형에서 부울대수형으로 변환  ﻿			
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "sym->bool";
+			}
+		});
+	    
+	    PrimDict.getInstance().put("bool->sym",new PrimWord() {
+			//부울대수형에서 심볼형으로 변환			
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "bool->sym";
+			}
+		});	
+	    
+	    PrimDict.getInstance().put("bool->str",new PrimWord() {
+ 			//부울대수형에서 문자열로 변환 		
+			@Override
+			public Object excute() {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				return null;
+			}
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "bool->str";
+			}
+		}); 
+      
+	 
 	}
 }
