6c5ab90257c302fcd93208bbacbff0112cf92916
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-08 13:46:26 +0000

    주석과 관련된 단어 이름을 바꾸고 주석 추가함!

--- src/CommentWords.java
+++ src/CommentWords.java
@@ -1,8 +1,8 @@
-
 public class CommentWords {
+//주석과 관련된 단어들이 정의됨
 		public CommentWords(){
-			PrimDict.getInstance().put("##",new PrimWord() {
-		
+			PrimDict.getInstance().put("!!~",new PrimWord() {
+		  // '!!~'를 입력시 그줄이 주석처리됨
 				@Override
 				public Object excute() throws Exception  {
 					// TODO Auto-generated method stub
@@ -10,27 +10,27 @@ public class CommentWords {
 					return null;
 				}
 				public String toString(){
-					return "##";
+					return "!!~";
 			}
 		});
 			
-			PrimDict.getInstance().put("#",new PrimWord() {
-				
+			PrimDict.getInstance().put("!~",new PrimWord() {
+		  // '!~'를 입력시 '~!'까지 주석처리됨				
 				@Override
 				public Object excute() throws Exception  {
 					// TODO Auto-generated method stub
-					SmingGo.getInstance().nextCharsUpTo('#');
+					SmingGo.getInstance().nextCharsUpTo('~!');
 					return null;
 				}
 				public String toString(){
-					return "#";
+					return "!~";
 			}
 		});
 			
-		    PrimDict.getInstance().get("##").im = true;
-		    PrimDict.getInstance().get("#").im = true;
+		    PrimDict.getInstance().get("!!~").im = true;
+		    PrimDict.getInstance().get("!~").im = true;
 		    
-		    PrimDict.getInstance().get("##").meaning = " -- ";
-		    PrimDict.getInstance().get("#").meaning = " -- ";			
+		    PrimDict.getInstance().get("!!~").meaning = " -- ";
+		    PrimDict.getInstance().get("!~").meaning = " -- ";			
 	}
 }
