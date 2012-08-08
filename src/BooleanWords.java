8328422139ec2d6a7a863a412b070861005414dc
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-08 13:19:20 +0000

    부울대수 연산자 관련 단어 주석 추가 및 일부 소스코드 수정 (반환x 스택에 직접 넣음)

--- src/BooleanWords.java
+++ src/BooleanWords.java
@@ -1,13 +1,14 @@
 public class BooleanWords {
-
+//부울대수와 관련한 기본 단어들 정의
 	public BooleanWords(){
 	    	
 		PrimDict.getInstance().put("true",new PrimWord() {	
-			
+			//부울대수 단어로써 스택에 true값을 넣음
 				@Override
 				public Object excute() {
 					// TODO Auto-generated method stub
-							return true;
+              DataStack.getInstance().push(true);
+							return null;
 				}
 				
 				@Override
@@ -18,11 +19,12 @@ public class BooleanWords {
 			});
 	     
 	     PrimDict.getInstance().put("false",new PrimWord() {
-				
+				//부울대수 단어로써 스택에 false값을 넣음
 				@Override
 				public Object excute() {
 					// TODO Auto-generated method stub
-							return false;
+              DataStack.getInstance().push(true);
+							return null;
 				}
 				
 				@Override
@@ -33,14 +35,15 @@ public class BooleanWords {
 			});
 	     
 	     PrimDict.getInstance().put("=?",new PrimWord() {
-				
+				//부울대수 연산자로 서로가 같은지 따져 물음.
 				@Override
 				public Object excute() {
 					// TODO Auto-generated method stub
 					Object obj2 = DataStack.getInstance().pop();
 					Object obj1 = DataStack.getInstance().pop();
 					
-					return obj1 == obj2;
+					DataStack.getInstance().push(obj1 == obj2);
+          return null;
 				}
 				
 				@Override
@@ -50,33 +53,17 @@ public class BooleanWords {
 				}
 			});
 	     
-	     PrimDict.getInstance().put("=?",new PrimWord() {
-				
-				@Override
-				public Object excute() {
-					// TODO Auto-generated method stub
-					Object obj2 = DataStack.getInstance().pop();
-					Object obj1 = DataStack.getInstance().pop();
-					
-					return obj1 == obj2;
-				}
-				
-				@Override
-				public String toString() {
-					// TODO Auto-generated method stub
-					return "=?";
-				}
-			});
 	     
 	     PrimDict.getInstance().put("!?",new PrimWord() {
-				
+ 			﻿ //부울대수 연산자로 서로가 다른지 따져 물음.
 				@Override
 				public Object excute() {
 					// TODO Auto-generated method stub
 					Object obj2 = DataStack.getInstance().pop();
 					Object obj1 = DataStack.getInstance().pop();
-					
-					return obj1 != obj2;
+					 
+          DataStack.getInstance().push(obj1 != obj2);
+          return null;
 				}
 				
 				@Override
@@ -87,6 +74,7 @@ public class BooleanWords {
 			});
 	     
 	     PrimDict.getInstance().put("and",new PrimWord() {
+ 			﻿ //부울대수 연산자로 서로가 'true' 일 경우 'true'를 스택에 넣음.
 				
 				@Override
 				public Object excute() throws Exception{
@@ -98,7 +86,8 @@ public class BooleanWords {
 						Boolean bool1 = (Boolean)obj1;
 						if (obj2 instanceof Boolean) {	
 							Boolean bool2 = (Boolean)obj2;
-							return bool1.booleanValue() && bool2.booleanValue();
+							DataStack.getInstace().push(bool1.booleanValue() && bool2.booleanValue());      
+							return null;
 						}
 						else{
 							Error.errorBool("두번째","and");
@@ -120,7 +109,7 @@ public class BooleanWords {
 			});
 	     
 	     PrimDict.getInstance().put("or",new PrimWord() {
-				
+       //부울대수 연산자로 둘중하나가 'true' 일 경우 'true'를 스택에 넣음.			
 				@Override
 				public Object excute() throws Exception{
 					// TODO Auto-generated method stub
@@ -131,7 +120,8 @@ public class BooleanWords {
 						Boolean bool1 = (Boolean)obj1;
 						if (obj2 instanceof Boolean) {	
 							Boolean bool2 = (Boolean)obj2;
-							return bool1.booleanValue() || bool2.booleanValue();
+							DataStack.getInstace().push(bool1.booleanValue() || bool2.booleanValue());      
+							return null;
 						}
 						else{
 							Error.errorBool("두번째","or");
@@ -153,7 +143,7 @@ public class BooleanWords {
 			});
 	     
 	     PrimDict.getInstance().put("not",new PrimWord() {
-				
+				//부울대수 연산자로 스택에서 빼온 부울대수 값을 바꿈.
 				@Override
 				public Object excute() throws Exception{
 					// TODO Auto-generated method stub
@@ -161,8 +151,9 @@ public class BooleanWords {
 					
 					if (obj1 instanceof Boolean) {	
 						Boolean bool1 = (Boolean)obj1;
-						
-							return !bool1.booleanValue();
+                  
+            DataStack.getInstace().push(!bool1.booleanValue());      
+						 return null;
 					}
 					else{
 						Error.errorBool("이","not");
