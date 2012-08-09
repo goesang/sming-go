18c59d676db79c5f988d4a9b0a7769cddc92226f
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-09 13:23:43 +0000

    주석 및 기본 단어들 생성!!!

--- src/IOWords.java
+++ src/IOWords.java
@@ -7,19 +7,20 @@ import java.io.FileWriter;
 
 public class IOWords {
 	public IOWords(){
-    	PrimDict.getInstance().put("read",new PrimWord() {
-			
+   //파일을 다루는 단어들 모음!
+    	PrimDict.getInstance().put("file-easy-read",new PrimWord() {
+			//단순히 파일을 읽어와서 문자열로 값을 내놓음
 			@Override
 			public Object excute() throws Exception{
 				// TODO Auto-generated method stub
-				Error.enoughMsg(1,"read");
+				Error.enoughMsg(1,"file-easy-read");
 				Object obj = DataStack.getInstance().pop();
-				if (obj instanceof String) {
-					String str = (String) obj;
+				if (obj instanceof Symbol) {
+					Symbol sym = (Symbol) obj;
 					try{
-						FileReader fr = new FileReader(str);
-						BufferedReader br = new BufferedReader(fr);
-						StringBuilder fStr = new StringBuilder();
+						FileReader fr = new FileReader(sym.toString()); // 파일 리더 만듬
+						BufferedReader br = new BufferedReader(fr);    // 버퍼 리더로 감쌈
+						StringBuilder fStr = new StringBuilder();     // 옮겨 적음 스트링빌더 생성
 						do {
 							String strTmp = br.readLine();
 							if(strTmp == null) break;
@@ -30,56 +31,109 @@ public class IOWords {
 						fr.close();
 					}
 					catch(Exception e){
-						throw new Exception("\"" + str+"\"이란 파일은 존재하지 않습니다! -- \"read\"");
+						throw new Exception("\"" + str+"\"이란 파일은 존재하지 않습니다! -- \"file-easy-read\"");
 					}
 				}
 				else{
-					Error.errorString("이","read");
+					Error.errorString("이","file-easy-read");
 				}
 				return null;
 
 			}
 			public String toString(){
-				return "read";
+				return "file-easy-read";
 			}
 		});
     	
-    	PrimDict.getInstance().put("write",new PrimWord() {
-			
+    	PrimDict.getInstance().put("file-easy-write",new PrimWord() {
+			//단순히 문자열을 파일에 씀
 			@Override
 			public Object excute() throws Exception{
 				// TODO Auto-generated method stub
-				Error.enoughMsg(2,"write");
+				Error.enoughMsg(2,"file-easy-write");
 				Object obj2 = DataStack.getInstance().pop();
 				Object obj1 = DataStack.getInstance().pop();
-				if (obj1 instanceof String) {
-					String str1 = (String) obj1;
+				if (obj1 instanceof Symbol) {
+					Symbol sym = (Symbol) obj1;
 					if (obj2 instanceof String) {
-						String str2 = (String) obj2;
+						String str = (String) obj2;
 						try{
-							FileWriter fw = new FileWriter(str1);
+							FileWriter fw = new FileWriter(sym.toString());
 							BufferedWriter bw = new BufferedWriter(fw);
 							bw.write(str2);
 							bw.close();
 							fw.close();
 						}
 						catch(Exception e){
-							throw new Exception("파일을 저장하는데 오류가 발생했습니다! -- \"write\"");
+							throw new Exception("파일을 저장하는데 오류가 발생했습니다! -- \"file-easy-write\"");
 						}
 					}
 					else{
-						Error.errorString("두번째","write");
+						Error.errorString("두번째","file-easy-write");
 					}
 				}
 				else{
-					Error.errorString("첫번째","write");
+					Error.errorString("첫번째","file-easy-write");
 				}
 				return null;
 
 			}
 			public String toString(){
-				return "read";
+				return "file-easy-write";
 			}
 		});
+      
+       PrimDict.getInstance().put("file-open",new PrimWord() {
+			//단순히 문자열을 파일에 씀
+			@Override
+			public Object excute() throws Exception{
+				// TODO Auto-generated method stub
+			 
+
+			}
+			public String toString(){
+				return "file-open";
+			}
+		});
+      
+       PrimDict.getInstance().put("file-read",new PrimWord() {
+			//단순히 문자열을 파일에 씀
+			@Override
+			public Object excute() throws Exception{
+				// TODO Auto-generated method stub
+			 
+
+			}
+			public String toString(){
+				return "file-read";
+			}
+		});
+      
+      PrimDict.getInstance().put("file-write",new PrimWord() {
+			//단순히 문자열을 파일에 씀
+			@Override
+			public Object excute() throws Exception{
+				// TODO Auto-generated method stub
+			 
+
+			}
+			public String toString(){
+				return "file-write";
+			}
+		});
+      
+      PrimDict.getInstance().put("file-close",new PrimWord() {
+			//단순히 문자열을 파일에 씀
+			@Override
+			public Object excute() throws Exception{
+				// TODO Auto-generated method stub
+			 
+
+			}
+			public String toString(){
+				return "file-close";
+			}
+		});
+      
 	}
 }
