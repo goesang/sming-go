9090e19503d7f6abedc33fd3e7e5346545c16d2e
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-24 14:37:45 +0000

    단어장 이라는 개념을 추가하여 검색함!!

--- src/UserDict.java
+++ src/UserDict.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 public class UserDict {
 	
 	private HashMap<String, UserWord> userDict;
-	
+	 //  private HashMap<String, HashMap> userDict;
+      
 	  private static UserDict single = new UserDict();
 	  
 	  public static UserDict getInstance(){
@@ -14,9 +15,32 @@ public class UserDict {
 	private UserDict(){
 		this.userDict = new HashMap<String, UserWord>();
 	}
-	public void put(String str,UserWord func){
-		this.userDict.put(str,func);
+   /*
+   	private UserDict(){
+		this.userDict = new HashMap<String, HashMap>();
+    
 	}
+   */
+	public void put(String str,UserWord word){
+		this.userDict.put(str,word);
+	}
+  /*
+ 	public void putVoca(String str,HashMap voca){
+		this.userDict.put(str,voca);
+	} 
+   
+   public void putWord(String wordName,UserWord word){
+   HashMap<String, HashMap> stdVoca = new HashMap<String, HashMap>();
+   stdVoca.put(wordName,voca);
+   
+		this.userDict.put("standard",stdVoca );
+	} 
+   
+   	public UserWord get(String vocaName , String wordName){
+		return this.userDict.get(vocaName).get(wordName);
+	}
+	
+   */
 	public UserWord get(String str){
 		return this.userDict.get(str);
 	}
