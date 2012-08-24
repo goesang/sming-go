e780f5d89f9edf2d3ffc5c2297cec576bc79c0ab
rootroot2@gmail.com <rootroot2@gmail.com@b7550929-e62f-c52b-6287-f2ac95803a37>
2012-08-24 14:18:00 +0000

    새로운 프로세싱 창

---
+++ src/NprocessingWindow.java
@@ -0,0 +1,48 @@
+import processing.core.PApplet;
+
+public class NprocessingWindow extends PApplet {
+
+
+ public void setup() {
+
+ }
+
+ public void draw() {
+
+ }
+
+ public void mousePressed() {
+  
+ }
+ 
+ public void mouseClicked() {
+
+}
+ public void mouseReleased() {
+  
+ }
+ 
+ public void mouseMoved() {
+
+}
+
+ public void mouseDragged() {
+
+}
+
+ public void mouseReleased() {
+  
+ }
+ 
+ public void mouseMoved() {
+
+}
+
+ public void mouseDragged() {
+
+}
+
+ static public void main(String args[]) {
+  PApplet.main();
+ }
+}
